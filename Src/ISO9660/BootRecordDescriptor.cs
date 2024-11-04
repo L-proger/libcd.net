89644bd23b8935d01dfc98750e9e9119f1ff908d
l-proger <l-proger@gmail.com>
2024-11-05 00:09:54 +0300

    Add volume descriptors

---
+++ Src/ISO9660/BootRecordDescriptor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibCD.ISO9660 {
+    public struct BootRecordDescriptor {
+        public VolumeDescriptorHeader Header;
+
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
+        public string BootSystemIdentifier; //a-characters
+
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
+        public string BootIdentifier; //a-characters
+
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 1977)]
+        public byte[] BootSystemUse; 
+    }
+}
